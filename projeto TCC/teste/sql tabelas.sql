cda5a6d7996cdfe8783d988f830f64a8d3bc00a9
Lucas Costa <88509075+lucascosta9349@users.noreply.github.com>
2024-11-07 15:02:36 -0300

    Update sql tabelas.sql

--- projeto TCC/teste/sql tabelas.sql
+++ projeto TCC/teste/sql tabelas.sql
@@ -1,4 +1,5 @@
-drop table placas;
+create database OCR;
+use OCR;
 create table Placas(
 placa varchar(7) primary key not null,
 dono_do_veiculo varchar(50) not null,
@@ -7,3 +8,5 @@ cargo varchar(50)
 );
 
 insert into placas (placa, dono_do_veiculo, telefone) values ("FBI5551", "lucas costa", "91992415502");
+insert into placas (placa, dono_do_veiculo, telefone) values ("DQE2H66", "Gustavo Nascimento", "19999547856");
+insert into placas (placa, dono_do_veiculo, telefone) values ("FBI5551", "wandrel alves", "19987541618");
